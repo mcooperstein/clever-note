c345be46ef412a29e83899f32bd0e2053f13ef74
Marc Cooperstein <mcooperstein@gmail.com>
2018-07-20 13:58:50 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: 500
+        }}
+      >
+        <h1>Welcome to CleverNote</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
